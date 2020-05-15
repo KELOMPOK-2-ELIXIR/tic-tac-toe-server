9f4b0180dae685bab8961fda042a67d3edebbd8f
hmyslf <yosa.alfq4@gmail.com>
2020-05-15 12:14:53 +0700

    add route

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,11 @@ const express = require("express");
 const router = express.Router();
 const ControllerUser = require("../controllers/controlleruser.js");
 
+router.get('/', (req, res) => {
+  res.status(200).json({
+    message: "Tic Tac Toe Server"
+  })
+});
 router.post('/signup', ControllerUser.signup);
 router.post('/data', ControllerUser.findall);
 router.put('/data', ControllerUser.updatedata);
